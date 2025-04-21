5a60cc5192c7413e5e5af537477fc526e9f5e4d0
Harold <haroooldg@gmail.com>
2025-04-21 15:03:16 -0500

    Add SwiftFormat step to CI workflow and format appDelegate.swift

--- iosChallenge/appDelegate.swift
+++ iosChallenge/appDelegate.swift
@@ -1,8 +1,10 @@
 import UIKit
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         return true
     }
 }
